4bb1bece8d2fface902edab9d76bbe1e67c03713
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-13 14:48:51 +0530

    Add OS identification Support and native Dialogs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,21 @@
 <script>
   import { invoke } from "@tauri-apps/api/core"; 
+  import { platform } from '@tauri-apps/plugin-os';
+
+  import { message } from '@tauri-apps/plugin-dialog';
+
+  
 
   let name = $state("");
   let greetMsg = $state("");
-
+  const currentPlatform = platform();
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     greetMsg = await invoke("greet", { name });
+  }
+  
+  async function native_dialog(){
+    await message('Hello This is a native Dialog', { title: 'PrintAssist Information', kind: 'warning' });
   }
 </script>
 
@@ -15,7 +24,9 @@
   <form class="flex justify-evenly space-x-4" onsubmit={greet}>
     <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
     <button type="submit" class="bg-gray-300 p-4">Greet</button>
+    <button onclick={native_dialog} class="bg-gray-300 p-4">Dialog</button>
   </form>
+  <p class="text-xl">Your Platform: {currentPlatform}</p>
   <p class="text-xl">{greetMsg}</p>
 </main>
 
